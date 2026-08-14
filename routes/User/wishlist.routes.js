b543acb96ee7d1678ba60f8dcdcb8d5556d7e338
Kenil codzcart <kenil.codzcartinfotech@gmail.com>
2026-08-14 12:35:33 +0530

    wishlist update 1

--- routes/User/wishlist.routes.js
+++ routes/User/wishlist.routes.js
@@ -7,13 +7,13 @@ router.post('/', wishlistController.addToWishlist);
 
 router.get('/', wishlistController.getWishlist);
 
+router.delete('/clear', wishlistController.clearWishlist);
+router.delete('/', wishlistController.clearWishlist);
+
 router.delete('/remove/:itemId', wishlistController.removeFromWishlist);
 router.delete('/:itemId', wishlistController.removeFromWishlist);
 
 router.post('/move-to-cart/:itemId', wishlistController.moveToCart);
 router.post('/:itemId/move-to-cart', wishlistController.moveToCart);
-
-router.delete('/clear', wishlistController.clearWishlist);
-router.delete('/', wishlistController.clearWishlist);
 
 module.exports = router;
